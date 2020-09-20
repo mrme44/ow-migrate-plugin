636490c1c1cee8dd490fd7b48a26d0f31846dea9
Mr. Me <russelljamison44@gmail.com>
2020-09-19 23:52:36 -0600

    Update ow-migrate-plugin.php
    Allow bypass migration lockout with get param or cookie

--- ow-migrate-plugin.php
+++ ow-migrate-plugin.php
@@ -1,101 +1,36 @@
 <?php
 /*
-Plugin Name: WordPress Overwatch Utilities
+Plugin Name: WordPress Overwatch Maintenance Mode for Website Migrations
 Plugin URI: https://wp-overwatch.com
-Description: This plugin helps us better manage your website
-Version: 1.0.0
+Description: This plugin puts the backend in maintenance mode while it is being migrated
+Version: 1.1.0
 Author: WP Overwatch
 Author URI:  http://wp-overwatch.com
 Contributors: Russell, Scotty
-
-Thanks to the developers of https://github.com/norcross/reaktiv-remote-repo
  */
-
-//TODO MOVE ALL THIS STUFF TO SEPERATE FILES
-
-
-//SETUP STUFF standard is to place this in a file called options.php
-//CREATE THE MENU PAGE
-
-if ( is_admin() ){
- function add_wp_overwatch_admin_menu() {
- 	add_options_page( 'WP Overwatch Options', 'WP Overwatch', 'manage_options', 'wp-overwatch', 'wp_overwatch_settings_page_callback' );
- }
- add_action( 'admin_menu', 'add_wp_overwatch_admin_menu' );
-
-	function register_wp_overwatch_settings() { // whitelist options
-	  register_setting( 'wp-option-group', 'new_option_name' );
-	  register_setting( 'wp-option-group', 'some_other_option' );
-	  register_setting( 'wp-option-group', 'option_etc' );
-	}
-	add_action( 'admin_init', 'register_wp_overwatch_settings' );
-
-	function wp_overwatch_settings_page_callback() {
-  	if ( !current_user_can( 'manage_options' ) )  {
-  		wp_die( __( 'The WP Overwatch plugin says hi. Unfortunately, it also says that you do not have sufficient permissions to access this page.' ) )
-	 	?>
-			<div class="wrap">';
-			 	<h1>Options</h1>
-				<p>These options are for internal use, to aid us in managing your website.</p>
-				<p>I have no idea what this plugin will eventually look like, but we will continue to add utilites here as we create them.</p>
-				<p>Feel free to contact russell@wordpressoverwatch.com if you need help</p>
-
-				<form method="post" action="options.php">
-				<?php
-
-					settings_fields( 'wp-option-group' );
-					//this code will replace the form field we are inside of
-					do_settings_sections( 'wp-option-group' );
-				?>
-
-				<table class="form-table">
-	        <tr valign="top">
-	        <th scope="row">New Option Name</th>
-	        <td><input type="text" name="new_option_name" value="<?php echo esc_attr( get_option('new_option_name') ); ?>" /></td>
-	        </tr>
-
-	        <tr valign="top">
-	        <th scope="row">Some Other Option</th>
-	        <td><input type="text" name="some_other_option" value="<?php echo esc_attr( get_option('some_other_option') ); ?>" /></td>
-	        </tr>
-
-	        <tr valign="top">
-	        <th scope="row">Options, Etc.</th>
-	        <td><input type="text" name="option_etc" value="<?php echo esc_attr( get_option('option_etc') ); ?>" /></td>
-	        </tr>
-		    </table>
-
-		    <?php submit_button(); ?>
-
-				</form>
-
-			</div>
-		<?php
-	 }
-}
-
-} // end of is_admin()
-
 
 
 function admin_maintenace_mode() {
     global $current_user;
     get_currentuserinfo();
-    if($current_user->user_login != 'wpoverwatch') { ?>
-                        <style> .updated{margin:30px !important;} </style><?
-                        wp_die('<div id="message" class="updated"><p><b>Maintenance mode:</b> We are currently making updates. Everything will be online shortly :)</p></div>');
-                }
+    if ( $current_user->user_login == 'wpoverwatch' || isset($_GET['wpoverwatch']) || isset($_COOKIE['wpoverwatch']) ) {
+        return;
+    } else {
+      $msg = "Howdy. You're website is currently being migrated to a different webhost by WordPress Overwatch. Once the migration is complete you will be able to login again. If we forgot to disable maintenance mode, or if you notice any other problems, send an email to russell@wordpressoverwatch.com.";
+      echo '<style> .updated{margin:30px !important;} </style>';
+      wp_die('<div id="message" class="updated"><p><b>Maintenance mode:</b>'.$msg.'</p></div>');
+    }
 }
-//add_action('admin_head', 'admin_maintenace_mode');
+add_action('admin_head', 'admin_maintenace_mode');
 
 
 
 // UPDATER CODE
 
 // set our defined items
-DEFINE( 'RKV_UPDATE_URL', 'https://github.com/mrme44/ow-plugin.git' );
+DEFINE( 'RKV_UPDATE_URL', 'https://github.com/mrme44/ow-migrate-plugin.git' );
 DEFINE( 'RKV_ITEM', 'WordPress Overwatch Utilities' );
-DEFINE( 'RKV_VERS', '1.0.0' );
+DEFINE( 'RKV_VERS', '1.1.0' );
 
 // load the class if we haven't aready
 if ( ! class_exists( 'RKV_Remote_Updater' ) ) {
